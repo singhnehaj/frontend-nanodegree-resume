6c1edff765721d30ad00f1812b6c683a662b8602
singhnehaj <singhneha.j@gmail.com>
2015-04-29 16:05:09 -0500

    Update resumeBuilder.js
    learning replace string command

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
 ("#main").append([Neha]);
 var awesomeThoughts="I am Neha and I am Awesome";
 console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+console.log(awesomeThoughts);
+comsole.log(funThoughts);
